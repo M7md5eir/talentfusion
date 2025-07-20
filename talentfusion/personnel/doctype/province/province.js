ad504f8f3b1c4f97018251446f464df598c1753c
Mohamed Kheir <M7md5eir@gmail.com>
2025-07-20 19:18:05 +0300

    Update province.js

--- talentfusion/personnel/doctype/province/province.js
+++ talentfusion/personnel/doctype/province/province.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Mohamed Kheir and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("province", {
+// frappe.ui.form.on("Province", {
 // 	refresh(frm) {
 
 // 	},
