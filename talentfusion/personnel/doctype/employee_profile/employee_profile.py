b998a44e5245b679e30f4c7576a26d431deff87a
Mohamed Kheir <M7md5eir@gmail.com>
2025-07-21 19:19:32 +0300

    Update employee_profile.py

--- talentfusion/personnel/doctype/employee_profile/employee_profile.py
+++ talentfusion/personnel/doctype/employee_profile/employee_profile.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2024, Mohamed Kheir and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class EmployeeProfile(Document):
-	pass
+
+    def validate(self):
+        # دالة جمع الأسماء
+        self.emp_name = self.join_names(
+            self.get("1st_emp_name"),
+            self.get("2nd_emp_name"),
+            self.get("3rd_emp_name"),
+            self.get("4th_emp_name")
+        )
+
+    def remove_extra_spaces(self, text):
+        if not text:
+            return ""
+        return " ".join(text.strip().split())
+
+    def join_names(self, *args):
+        return " ".join([self.remove_extra_spaces(part) for part in args if part]).strip()
