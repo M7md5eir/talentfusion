f8bad9b2150578196839c2407903441cc3f25bc1
Mohamed Kheir <M7md5eir@gmail.com>
2025-07-21 09:13:57 +0300

    Add files via upload

--- talentfusion/others/doctype/employee_addresses/employee_addresses.py
+++ talentfusion/others/doctype/employee_addresses/employee_addresses.py
@@ -6,4 +6,27 @@ from frappe.model.document import Document
 
 
 class EmployeeAddresses(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		active: DF.Check
+		apartment: DF.Data | None
+		building: DF.Data | None
+		city: DF.Link | None
+		country: DF.Link | None
+		end_date: DF.Date | None
+		floor: DF.Data | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		postal_code: DF.Data | None
+		province: DF.Link | None
+		start_date: DF.Date | None
+		street: DF.Data | None
+	# end: auto-generated types
 	pass
---
+++ talentfusion/others/doctype/employee_addresses/test_employee_addresses.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mohamed Kheir and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmployeeAddresses(FrappeTestCase):
+	pass
