2efbf1087f6d6e655b0a39ffad2b488a1bf818e6
Mohamed Kheir <M7md5eir@gmail.com>
2025-07-21 09:16:16 +0300

    Delete talentfusion/personnel/doctype/employee_addresses directory

--- talentfusion/personnel/doctype/employee_addresses/test_employee_addresses.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Mohamed Kheir and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestEmployeeAddresses(FrappeTestCase):
-	pass
